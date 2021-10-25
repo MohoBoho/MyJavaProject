14d34124706363338372bd5d298cdb91477bf951
metometo <orbit_alkal@yahoo.com>
2021-10-25 09:23:44 +0300

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -1,5 +1,5 @@
 @echo off
 echo ---Deploying site
 
-REM ---Deploy the wwwroot folder in repository to default target (wwwroot)
-xcopy %DEPLOYMENT_SOURCE%\wwwroot\webapps\FirstWebApp\* %DEPLOYMENT_TARGET% /Y /s /E /H /C /I
+xcopy C:\home\site\wwwroot\webapps\FirstWebApp\ C:\home\site\wwwroot\webapps\ROOT\ /Y /s /E /H /C /I
+
